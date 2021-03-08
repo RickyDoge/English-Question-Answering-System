fb07878d479dd3e5468b8d825222b49b570d4722
s2060000 <s2060000@ed.ac.uk>
2021-03-08 02:53:55 +0000

    fix bug - divided zero

--- train.py
+++ train.py
@@ -25,36 +25,37 @@ def test(valid_iterator, model, device):
     f1_sum = 0  # F1-score
     f1_count = 0
 
-    for data in valid_iterator:
-        batch_encoding, is_impossibles, start_position, end_position, id = data
-        is_impossibles = utils.move_to_device(is_impossibles, device)
-        start_position = utils.move_to_device(start_position, device)
-        end_position = utils.move_to_device(end_position, device)
-        cls_out, start_logits, end_logits = model(batch_encoding['input_ids'].to(device),
-                                                  attention_mask=batch_encoding['attention_mask'].to(device),
-                                                  token_type_ids=batch_encoding['token_type_ids'].to(device),
-                                                  )
-        impossible_loss = cls_loss(cls_out, is_impossibles)
-        start_loss = start_end_loss(start_logits, start_position)
-        end_loss = start_end_loss(end_logits, end_position)
-        loss = start_loss + end_loss + impossible_loss
+    with torch.no_grad():
+        for data in valid_iterator:
+            batch_encoding, is_impossibles, start_position, end_position, id = data
+            is_impossibles = utils.move_to_device(is_impossibles, device)
+            start_position = utils.move_to_device(start_position, device)
+            end_position = utils.move_to_device(end_position, device)
+            cls_out, start_logits, end_logits = model(batch_encoding['input_ids'].to(device),
+                                                      attention_mask=batch_encoding['attention_mask'].to(device),
+                                                      token_type_ids=batch_encoding['token_type_ids'].to(device),
+                                                      )
+            impossible_loss = cls_loss(cls_out, is_impossibles)
+            start_loss = start_end_loss(start_logits, start_position)
+            end_loss = start_end_loss(end_logits, end_position)
+            loss = start_loss + end_loss + impossible_loss
 
-        loss_sum += loss.item()
-        loss_count += 1
+            loss_sum += loss.item()
+            loss_count += 1
 
-        predict_start = torch.argmax(start_logits, dim=-1)
-        predict_end = torch.argmax(end_logits, dim=-1)
+            predict_start = torch.argmax(start_logits, dim=-1)
+            predict_end = torch.argmax(end_logits, dim=-1)
 
-        cls_out = torch.argmax(cls_out, dim=-1)
-        cls_out = (cls_out == is_impossibles.argmax(dim=-1)).float()
-        cls_correct_count += torch.sum(cls_out)
-        cls_total_count += cls_out.size(0)
+            cls_out = torch.argmax(cls_out, dim=-1)
+            cls_out = (cls_out == is_impossibles.argmax(dim=-1)).float()
+            cls_correct_count += torch.sum(cls_out)
+            cls_total_count += cls_out.size(0)
 
-        for ps, pe, rs, re in zip(predict_start, predict_end, start_position, end_position):
-            recall = utils.calculate_recall(ps, pe, rs, re)
-            precision = utils.calculate_recall(rs, re, ps, pe)
-            f1_sum += (recall + precision) / 2
-            f1_count += 1
+            for ps, pe, rs, re in zip(predict_start, predict_end, start_position, end_position):
+                recall = utils.calculate_recall(ps, pe, rs, re)
+                precision = utils.calculate_recall(rs, re, ps, pe)
+                f1_sum += (recall + precision) / 2
+                f1_count += 1
 
     return loss_sum / loss_count, cls_correct_count / cls_total_count, f1_sum / f1_count
 
@@ -83,8 +84,6 @@ def main(epoch=10):
                                       collate_fn=partial(my_collate_fn, tokenizer=tokenizer))
     dataloader_valid = tud.DataLoader(dataset=dataset_valid, batch_size=64, shuffle=False,
                                       collate_fn=partial(my_collate_fn, tokenizer=tokenizer))
-    train_iterator = iter(dataloader_train)
-    valid_iterator = iter(dataloader_valid)
 
     # load pre-trained model
     model = BaselineModel()
@@ -118,7 +117,9 @@ def main(epoch=10):
     cls_loss = nn.BCELoss()  # Binary Cross Entropy Loss
     start_end_loss = nn.CrossEntropyLoss()
 
+    train_iterator = iter(dataloader_train)
     for e in range(epoch):
+        valid_iterator = iter(dataloader_valid)
         valid_loss, cls_acc, f1 = test(valid_iterator, model, device)
         logger.info('Epoch {}, Valid loss {}, Classification Acc {}, F1-score {}'.format(e, valid_loss, cls_acc, f1))
         for i, data in enumerate(train_iterator):
