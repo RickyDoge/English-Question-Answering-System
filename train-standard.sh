d816898fd66fbfedd08873ef87d99c70a95eb2ea
s2060000 <s2060000@ed.ac.uk>
2021-03-12 00:40:44 +0000

    Update train-standard.sh

--- train-standard.sh
+++ train-standard.sh
@@ -34,4 +34,4 @@ export DATASET_DIR=${TMP}/datasets/
 
 
 source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
-python train.py -c baseline-small -d small -w 0.8
+python train_baseline.py -c baseline-small -d small -w 0.4
