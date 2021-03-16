2a13e42816e99e95a0b20346ed64fc8f87ae54d1
s2060000 <s2060000@ed.ac.uk>
2021-03-16 13:10:50 +0000

    fix bug

--- train.py
+++ train.py
@@ -2,10 +2,8 @@ import torch
 import torch.nn as nn
 import torch.utils.data as tud
 import torch.optim as optim
-import os
 import logging
 import argparse
-from train_baseline import test
 from evaluate.evaluate import test_multi_task_learner
 from transformers import ElectraTokenizerFast
 from model import utils
@@ -15,44 +13,55 @@ from model.dataset import my_collate_fn, QuestionAnsweringDatasetConfiguration, 
 from functools import partial
 
 
-def test_intensive_reader(valid_iterator, model, device, pad_idx):
+def test(valid_iterator, model, device, tokenizer):
     model.eval()
+    cls_loss = nn.BCELoss()
     start_end_loss = nn.CrossEntropyLoss()
 
     loss_sum = 0  # loss
     loss_count = 0
+
+    cls_correct_count = 0  # is impossible
+    cls_total_count = 0
 
     f1_sum = 0  # F1-score
     f1_count = 0
 
     with torch.no_grad():
         for data in valid_iterator:
-            batch_encoding, _, start_position, end_position, _ = data
+            batch_encoding, is_impossibles, start_position, end_position, _ = data
+            is_impossibles = utils.move_to_device(is_impossibles, device)
             start_position = utils.move_to_device(start_position, device)
             end_position = utils.move_to_device(end_position, device)
-            start_position = torch.where(start_position >= 1, start_position - 1, start_position)
-            end_position = torch.where(end_position >= 1, end_position - 1, end_position)
+            # minus one, because we removed [CLS] when utils.generate_question_and_passage_hidden
+            start_position = torch.where(start_position > 1, start_position - 1, start_position)
+            end_position = torch.where(end_position > 1, end_position - 1, end_position)
             max_con_len, max_qus_len = utils.find_max_qus_con_length(attention_mask=batch_encoding['attention_mask'],
                                                                      token_type_ids=batch_encoding['token_type_ids'],
                                                                      max_length=batch_encoding['input_ids'].size(1),
                                                                      )
-            start_logits, end_logits = model(batch_encoding['input_ids'].to(device),
-                                             attention_mask=batch_encoding['attention_mask'].to(device),
-                                             token_type_ids=batch_encoding['token_type_ids'].to(device),
-                                             pad_idx=pad_idx,
-                                             max_qus_length=max_qus_len,
-                                             max_con_length=max_con_len,
-                                             )
+            cls_out, start_logits, end_logits = model(batch_encoding['input_ids'].to(device),
+                                                      attention_mask=batch_encoding['attention_mask'].to(device),
+                                                      token_type_ids=batch_encoding['token_type_ids'].to(device),
+                                                      pad_idx=tokenizer.pad_token_id,
+                                                      max_qus_length=max_qus_len,
+                                                      max_con_length=max_con_len,
+                                                      )
+            impossible_loss = cls_loss(cls_out, is_impossibles)
             start_loss = start_end_loss(start_logits, start_position)
             end_loss = start_end_loss(end_logits, end_position)
+            loss = (start_loss + end_loss) / 2 + impossible_loss
 
-            span_loss = (start_loss + end_loss) / 2
-
-            loss_sum += span_loss.item()
+            loss_sum += loss.item()
             loss_count += 1
 
             predict_start = torch.argmax(start_logits, dim=-1)
             predict_end = torch.argmax(end_logits, dim=-1)
+
+            cls_out = torch.argmax(cls_out, dim=-1)
+            cls_out = (cls_out == is_impossibles.argmax(dim=-1)).float()
+            cls_correct_count += torch.sum(cls_out)
+            cls_total_count += cls_out.size(0)
 
             predict_start = predict_start.cpu().numpy()
             predict_end = predict_end.cpu().numpy()
@@ -64,39 +73,7 @@ def test_intensive_reader(valid_iterator, model, device, pad_idx):
                 f1_sum += (recall + precision) / 2
                 f1_count += 1
 
-    model.train()
-    return loss_sum / loss_count, f1_sum / f1_count
-
-
-def test_sketch_reader(valid_iterator, model, device):
-    model.eval()
-    cls_loss = nn.BCELoss()
-    loss_sum = 0  # loss
-    loss_count = 0
-
-    cls_correct_count = 0  # is impossible
-    cls_total_count = 0
-
-    with torch.no_grad():
-        for data in valid_iterator:
-            batch_encoding, is_impossibles, _, _, _ = data
-            is_impossibles = utils.move_to_device(is_impossibles, device)
-            cls_out = model(batch_encoding['input_ids'].to(device),
-                            attention_mask=batch_encoding['attention_mask'].to(device),
-                            token_type_ids=batch_encoding['token_type_ids'].to(device),
-                            )
-            impossible_loss = cls_loss(cls_out, is_impossibles)
-
-            loss_sum += impossible_loss.item()
-            loss_count += 1
-
-            cls_out = torch.argmax(cls_out, dim=-1)
-            cls_out = (cls_out == is_impossibles.argmax(dim=-1)).float()
-            cls_correct_count += torch.sum(cls_out).item()
-            cls_total_count += cls_out.size(0)
-
-    model.train()
-    return loss_sum / loss_count, cls_correct_count / cls_total_count
+    return loss_sum / loss_count, cls_correct_count / cls_total_count, f1_sum / f1_count
 
 
 def main(epoch=4, which_config='cross-attention', which_dataset='small', multitask_weight=1.0, seed=2020):
@@ -129,9 +106,9 @@ def main(epoch=4, which_config='cross-attention', which_dataset='small', multita
         config_valid = QuestionAnsweringDatasetConfiguration(squad_dev=True)
     dataset_train = QuestionAnsweringDataset(config_train, tokenizer=tokenizer)
     dataset_valid = QuestionAnsweringDataset(config_valid, tokenizer=tokenizer)
-    dataloader_train = tud.DataLoader(dataset=dataset_train, batch_size=48, shuffle=True, drop_last=True,
+    dataloader_train = tud.DataLoader(dataset=dataset_train, batch_size=8, shuffle=True, drop_last=True,
                                       collate_fn=partial(my_collate_fn, tokenizer=tokenizer))
-    dataloader_valid = tud.DataLoader(dataset=dataset_valid, batch_size=48, shuffle=False, drop_last=True,
+    dataloader_valid = tud.DataLoader(dataset=dataset_valid, batch_size=8, shuffle=False, drop_last=True,
                                       collate_fn=partial(my_collate_fn, tokenizer=tokenizer))
 
     # load pre-trained model
@@ -200,15 +177,15 @@ def main(epoch=4, which_config='cross-attention', which_dataset='small', multita
                                                                      token_type_ids=batch_encoding['token_type_ids'],
                                                                      max_length=batch_encoding['input_ids'].size(1),
                                                                      )
-            cls_output, start_logits, end_logits = intensive_model(batch_encoding['input_ids'].to(device),
-                                                                   attention_mask=batch_encoding['attention_mask']
-                                                                   .to(device),
-                                                                   token_type_ids=batch_encoding['token_type_ids']
-                                                                   .to(device),
-                                                                   pad_idx=tokenizer.pad_token_id,
-                                                                   max_qus_length=max_qus_len,
-                                                                   max_con_length=max_con_len,
-                                                                   )
+            model_output = intensive_model(batch_encoding['input_ids'].to(device),
+                                           attention_mask=batch_encoding['attention_mask'].to(device),
+                                           token_type_ids=batch_encoding['token_type_ids'].to(device),
+                                           pad_idx=tokenizer.pad_token_id,
+                                           max_qus_length=max_qus_len,
+                                           max_con_length=max_con_len,
+                                           )
+
+            cls_output, start_logits, end_logits = model_output
             start_loss = start_end_loss(start_logits, start_position)
             end_loss = start_end_loss(end_logits, end_position)
             answerable_loss = cls_loss(cls_output, is_impossibles)
@@ -218,10 +195,10 @@ def main(epoch=4, which_config='cross-attention', which_dataset='small', multita
             loss.backward()
             optimizer.step()
             if i % 500 == 0:
-                logger.info('Epoch {}, Iteration {}, Span Loss: {:.4f}, Ans Loss{:.4f}'.format(e, i, printable[0],
+                logger.info('Epoch {}, Iteration {}, Span Loss: {:.4f}, Ans Loss {:.4f}'.format(e, i, printable[0],
                                                                                                printable[1]))
-                v_loss_intensive, acc, f1 = test(iter(dataloader_valid), intensive_model, device)
-                logger.info('Epoch {}, Iteration {}, Intensive valid loss {:.4f}, CLS acc{:.4f}, F1-score {:.4f}'
+                v_loss_intensive, acc, f1 = test(iter(dataloader_valid), intensive_model, device, tokenizer)
+                logger.info('Epoch {}, Iteration {}, Intensive valid loss {:.4f}, CLS acc {:.4f}, F1-score {:.4f}'
                             .format(e, i, v_loss_intensive, acc, f1))
                 score = acc * f1
                 if score >= best_score:  # save the best model
@@ -272,10 +249,10 @@ def main(epoch=4, which_config='cross-attention', which_dataset='small', multita
             loss.backward()
             optimizer.step()
 
-        logger.info('Epoch {}, Span Loss: {:.4f}, Ans Loss{:.4f}'
+        logger.info('Epoch {}, Span Loss: {:.4f}, Ans Loss {:.4f}'
                     .format(e, printable[0], printable[1]))
-        v_loss_intensive, acc, f1 = test(iter(dataloader_valid), intensive_model, device)
-        logger.info('Epoch {}, Intensive valid loss {:.4f}, CLS acc{:.4f}, F1-score {:.4f}'
+        v_loss_intensive, acc, f1 = test(iter(dataloader_valid), intensive_model, device, tokenizer)
+        logger.info('Epoch {}, Intensive valid loss {:.4f}, CLS acc {:.4f}, F1-score {:.4f}'
                     .format(e, v_loss_intensive, acc, f1))
         score = acc * f1
         if score >= best_score:  # save the best model
